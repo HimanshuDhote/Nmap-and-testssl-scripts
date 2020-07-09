1399e381ec85307dcd3747f74e68d340d12533c8
Himanshu <himanshu.dhote03@gmail.com>
2020-07-09 09:17:54 -0400

    Basic namp scan and testssl tool to scan ssl vuln

---
+++ nmaptester.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author : Himanshu Dhote 
+
+echo " 
+=========================================
+=      The Nmap Scanner                 =
+=                                       =
+=                   -by Himanshu Dhote  =
+========================================= "
+
+read -p 'Enter target IP address: ' target
+#read target
+$nmapType nmap -sC -sV -vv --script ssh2-enum-algos -oA Basic_"$target".nmap "$target" 
+bash /home/kali/Downloads/testssl.sh-3.1dev/testssl.sh --warnings=batch --html "$target"
